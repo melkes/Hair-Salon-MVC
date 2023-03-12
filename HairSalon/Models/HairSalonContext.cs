c099e5ed84cd619f73fd3e736d2752770f63b212
Mike Wilkes <mikewilkes@outlook.com>
2023-03-11 22:55:09 -0800

    Added Shared and Home views, updated Stylist.cs to correct plural/singular namings

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,7 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    public DbSet<Stylists> Stylists { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
     // public DbSet<Clients> Clients { get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
   {
     public int StylistId { get; set; }
     public string Name { get; set; }
-    public List<Stylists> Stylists { get; set; }
+    // public List<Stylists> Stylists { get; set; } Do I need something like this?
   }
 
 }
